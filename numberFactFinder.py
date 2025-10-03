1a35ad722dd97ae1c19437ba126c31226b59b85e
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2025-10-03 18:06:56 -0500

    going to take a break

--- numberFactFinder.py
+++ numberFactFinder.py
@@ -3,3 +3,16 @@ import sys
 
 #Global API
 API_URL = "http://numbersapi.com"
+
+# Get number function
+def get_number_fact(number):
+    
+    endpoint = f"{API_URL}\{number}/trivia"
+
+    print(f"\nSearching for a fact about {number}...")
+
+    try:
+        response = requests.get(endpoint)
+
+    except Exception as e:
+        return f"An unexpected internal error occurred: {e}"
